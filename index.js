3d68e73a33e180e7fa1520c1a2a9b0683174f90c
Holger Will <h.will@klimapartner.de>
2016-08-25 09:31:30 +0200

    don't fire "click" events if there was no "down" event before "up"

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-function ButtonAktor (app){
-  this.active=""
+function ButtonActuator (app){
+  this.active="unknown"
   this.id=""
   this.down = function(){}
   this.up = function(){}
   this.click = function(){}
-  this.addEventListener=function(event,callback){
-    this[event]=callback
-  }
   app.on("known-data",function(data){
     if(data.senderId==this.id){
       var val = data.values[0].value
       var ba = val.split(" ")
-
       if(ba[0]=="released")  {
-        this.up({button:this.active})
-        this.click({button:this.active})
-        this.active=""
+        this.up({button:this.active,event:"down"})
+        if(this.active!="unknown") this.click({button:this.active,event:"click"})
+        this.active="unknown"
         return
       }
       this.active = ba[0]
-      this.down({button:this.active})
+      this.down({button:this.active,event:"up"})
     }
   }.bind(this))
 }
-module.exports=ButtonAktor
+module.exports=ButtonActuator
